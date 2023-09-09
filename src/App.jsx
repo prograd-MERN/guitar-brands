501830f484d594f5dd65de5e10a58c9dfd61ddea
Balaramansethu <baalaaraman@gmail.com>
2023-09-09 17:30:30 +0530

    ADD.added css and source code for changing guitar model name.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import GuitarComponents from './components/GuitarComponents/GuitarComponents'
-
 function App() {
+
+
   return (
-<React.Fragment> 
+<React.Fragment>
   <GuitarComponents/>
 </React.Fragment> )
 }
--- src/components/GuitarComponents/GuitarComponents.jsx
+++ src/components/GuitarComponents/GuitarComponents.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import React,{useState} from 'react'
 const GuitarComponents = () => {
+
+    const [guitar,setGuitar]=useState({
+        Brand :'yamaha',
+        Model :'electra'})
+
   return (
-    <div>GuitarComponents</div>
+    <React.Fragment>
+            <div id='whole-container'><span>If you want to change the guitar model click here --- </span>
+            <button onClick={  ()=>setGuitar({...guitar,Model:'stanminos'})}>ChangeModel</button>
+  <p>{guitar.Brand}</p>
+  <p>{guitar.Model}</p>
+  </div>
+    </React.Fragment>
   )
 }
 
